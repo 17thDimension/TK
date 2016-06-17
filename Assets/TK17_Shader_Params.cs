166b2a6bc63d52bbeffa20ef545346736a90c8f0
17th Dimension <hello@paulwand.com>
2016-06-17 16:05:16 -0700

    first commit

---
+++ Assets/TK17_Shader_Params.cs
@@ -0,0 +1,36 @@
+﻿namespace TK17
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.Rendering;
+
+	[ExecuteInEditMode]
+	public class TK17_ShaderParams : MonoBehaviour
+	{
+		[System.Serializable]
+		public class StringBoolPair
+		{
+			public string key;
+			public bool value;
+		}
+
+		[System.Serializable]
+		public class StringFloatPair
+		{
+			public string key;
+			public float value;
+		}
+
+		public List<StringFloatPair> floatparams = new List<StringFloatPair>();
+
+		public void AssignParams(ref Material mat)
+		{
+			for (int i = 0; i < floatparams.Count; ++i)
+			{
+				mat.SetFloat(m_fparams[i].key, m_fparams[i].value);
+			}
+			renderer.SetPropertyBlock(m_mpb);
+		}
+	}
+}
