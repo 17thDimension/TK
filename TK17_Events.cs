6309017e41d81871d143fb73f7441149b84c9afc
17th Dimension <hello@paulwand.com>
2016-06-17 16:27:24 -0700

    nuke everything but the CODE

---
+++ TK17_Events.cs
@@ -0,0 +1,25 @@
+﻿namespace TK17
+{
+	using UnityEngine;
+
+	public struct ShaderParamChangeEventArgs
+	{
+		public TK17_ShaderParams shaderparams;
+	}
+
+    public delegate void ShaderParamsChangedEventHandler(object sender, ShaderParamChangeEventArgs e);
+
+    public class TK17_Events : MonoBehaviour
+	{
+
+		public event ShaderParamsChangedEventHandler ShaderParamsChanged;
+
+        public virtual void OnShaderParamChanged(ShaderParamChangeEventArgs e)
+		{
+			if (ShaderParamsChanged != null)
+				ShaderParamsChanged(this, e);
+		}
+
+
+	}
+}
